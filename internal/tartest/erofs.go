c58b5853004eb3b8bdd327d3d80b8d2300b0b23d
Kern Walster <kern.walster@gmail.com>
2026-03-04 23:05:47 -0800

    Generate test data from tar file
    This change switches from committed binary test data to generating a tar
    file on demand and then converting the tar file to an erofs file using
    mkfs.erofs.
    
    Signed-off-by: Kern Walster <kern.walster@gmail.com>

---
+++ internal/tartest/erofs.go
@@ -0,0 +1,39 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package tartest
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os/exec"
+)
+
+func ConvertTarErofs(ctx context.Context, r io.Reader, layerPath, uuid string, mkfsExtraOpts []string) error {
+	args := append([]string{"--tar=f", "--aufs", "--quiet", "-Enoinline_data"}, mkfsExtraOpts...)
+	if uuid != "" {
+		args = append(args, []string{"-U", uuid}...)
+	}
+	args = append(args, layerPath)
+	cmd := exec.CommandContext(ctx, "mkfs.erofs", args...)
+	cmd.Stdin = r
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("erofs apply failed: %s: %w", out, err)
+	}
+	return nil
+}
